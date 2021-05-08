4a12afada67ab737b70c5db66d351a3afc21cac0
Luke Stocker <lukestocker1@gmail.com>
2021-05-08 14:43:46 -0400

    task 2.3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const mainEl = document.querySelector('main');
 const topMenuEl = document.querySelector('nav');
 
+//task1
 let color = "var(--main-bg)"
 mainEl.style.background = color;
 
@@ -9,5 +10,8 @@ mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
 mainEl.classList.add('flex-ctr');
 
 //task 2
-
 topMenuEl.style.height = "100%";
+
+topMenuEl.style.background = "var(--top-menu-bg)"
+
+topMenuEl.classlist.add("flex-around");
