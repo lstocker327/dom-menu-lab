4eabdc469f3f79bf42a74dc765c50cf8aa576aae
Luke Stocker <lukestocker1@gmail.com>
2021-05-08 14:25:46 -0400

    JS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const mainEl = document.querySelector('main');
+
+let color = "var(--main-bg)"
+mainEl.style.background = color;
+
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+
+mainEl.classList.add('flex-ctr');
+//hello
