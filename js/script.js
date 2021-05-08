a04845cb0bb5ede7e67d552303c1bc8c044f8c30
Luke Stocker <lukestocker1@gmail.com>
2021-05-08 15:13:49 -0400

    task 3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// Menu data structure
+var menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '/catalog'},
+  {text: 'orders', href: '/orders'},
+  {text: 'account', href: '/account'},
+];
+//cached elements
 const mainEl = document.querySelector('main');
 const topMenuEl = document.querySelector('nav');
 
@@ -14,4 +22,12 @@ topMenuEl.style.height = "100%";
 
 topMenuEl.style.background = "var(--top-menu-bg)"
 
-topMenuEl.classlist.add("flex-around");
+topMenuEl.classList.add("flex-around");
+
+//task 3
+menuLinks.forEach(function(link){
+  const a = document.createElement('a');
+  a.setAttribute('href', link.href);
+  a.innerHTML = link.text;
+  topMenuEl.appendChild(a);
+});
